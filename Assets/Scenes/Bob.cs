d07a7b64b97fb487b9221c7ba1a07a30959d9040
Richu76 <richupop76@gmail.com>
2024-01-18 20:01:21 -0600

    added bob, made some background changes

--- Assets/Scenes/Bob.cs
+++ Assets/Scenes/Bob.cs
@@ -3,16 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Madak : Enemy
+public class Bob : Enemy
 {
-    // Start is called before the first frame update
     protected override void Start()
     {
         base.Start();
-        health = 15000;
-        damage = 900;
-        speed = 1;
-        goldDrops = 1000;
+        health = 100000;
+        damage = 100;
+        speed = 2;
+        goldDrops = 10000;
     }
-
 }
