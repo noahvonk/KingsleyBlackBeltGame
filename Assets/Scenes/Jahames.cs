c1369cdf54811e270ffbf0235d2b7cc2d62263e4
Richu76 <richupop76@gmail.com>
2024-02-01 20:00:18 -0600

    Jahames and bob fixed

---
+++ Assets/Scenes/Jahames.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jahames : Enemy
+{
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+        health = 50000;
+        damage = 300;
+        speed = 15;
+        goldDrops = 1000;
+    }
+
+}
